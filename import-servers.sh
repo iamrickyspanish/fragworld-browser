f82b26f53f2ff11704090c0249c942855c4a7a3b
Enrico <enrico@hrlab.de>
2022-02-22 00:55:12 +0100

    .

--- import-servers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mongoimport --db fragworld-browser --collection servers \
-          --authenticationDatabase admin --username root --password root \
-          --drop --file ./import.json --host 0.0.0.0 --port 27017
